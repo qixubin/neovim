e09e1478b3b998a6362d44511d4d7bb2ec2e516c
qixubin <qixubin@gmail.com>
2022-09-14 01:07:12 +0800

    add haskell lsp

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua" }
+local servers = { "jsonls", "sumneko_lua" , "hls"}
 
 lsp_installer.setup({
 	ensure_installed = servers,
